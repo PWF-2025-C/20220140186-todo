7c61c7d01cb08e60f1f8ab7aeec1a712c1cff549
cahyadie <cahyadiilham12@gmail.com>
2025-03-15 16:19:01 +0700

    env, mvc, route, relationship

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+    Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+});
+
+require __DIR__.'/auth.php';
