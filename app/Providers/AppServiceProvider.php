0a9b358a1187a70281d998f5a0f73df6f567d520
cahyadie <cahyadiilham12@gmail.com>
2025-06-04 18:12:05 +0700

    api : auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,14 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Str;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +30,11 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('admin', function ($user) {
             return $user->is_admin == true;
         });
+        // Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+
+        Scramble::configure()->routes(function (Route $route) {
+            return Str::startsWith($route->uri, 'api/');
+        });
+        
     }
 }
---
+++ tugas11.md
@@ -0,0 +1,5 @@
+# Tugas 11
+
+## 1. Request Api
+
+![Alt text](screenshoot/tugas11/image1.png)
